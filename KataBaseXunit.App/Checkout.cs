83407a01b896cac18c3f2be6544709d781740ac8
Vince Lee <vince.lee@polyhatsoftware.co.uk>
2021-03-18 19:21:45 +0000

    SingleACosts50 - green

--- KataBaseXunit.App/Checkout.cs
+++ KataBaseXunit.App/Checkout.cs
@@ -1,16 +1,31 @@
-﻿namespace KataBaseXunit.App
+﻿using System.Collections.Generic;
+
+namespace KataBaseXunit.App
 {
     public class Checkout
     {
+        private List<string> _items;
+
+        public Checkout()
+        {
+            _items = new List<string>();
+        }
+
         public int GetTotalPrice()
         {
             var totalPrice = 0;
+            
+            foreach (var item in _items)
+            {
+                if (item == "A") totalPrice = 50;
+            }
+            
             return totalPrice;
         }
 
         public void Scan(string s)
         {
-            throw new System.NotImplementedException();
+            _items.Add(s);
         }
     }
 }
